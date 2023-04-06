9fb3d7a5270e7c8815918c07f0416d06923bc72f
김민정 <mjkim@ipartners.co.kr>
2023-04-06 16:04:30 +0900

    router 수정

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 
 
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HeaderNav from 'components/Header/HeaderNav';
+import { Routes, Route } from 'react-router-dom';
 import { AuthContextProvider } from 'components/context/AuthContext';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import NotFound from 'pages/NotFound';
 import Home from 'pages/Home';
 import AllProducts from 'pages/products/AllProducts';
-import MyCart from 'pages/carts/MyCart';
+import MyCart from 'pages/MyCart';
+import ProtectedRouter from 'pages/ProtectedRouter';
+import NewProdut from 'pages/products/NewProduct';
+import ProdeuctDetail from 'pages/products/ProdeuctDetail';
+import HeaderNav from 'components/Header/HeaderNav';
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-        <BrowserRouter>
           <HeaderNav />
           <Routes>
             <Route path="/" exact element={<Home />}></Route>
             <Route path="/products" element={<AllProducts />}></Route>
-            <Route path="/carts" element={<MyCart />}></Route>
+            <Route path="/products/new" element={
+               <ProtectedRouter requireAdmin={true} >
+                <NewProdut />
+              </ProtectedRouter>
+            }></Route>
+            <Route path="/products/:id" element={
+                <ProdeuctDetail />
+            }></Route>
+            <Route path="/carts" element={
+              <ProtectedRouter>
+                <MyCart />
+              </ProtectedRouter>
+            }></Route>
             <Route path="*" element={<NotFound />}></Route>
           </Routes>
-        </BrowserRouter>
-        {/* <Outlet /> */}
       </AuthContextProvider>
     </QueryClientProvider>
   );
