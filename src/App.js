96bf7a1cdb2073eeaefa8503c3f656a7e9a57910
김민정 <mjkim@ipartners.co.kr>
2023-04-06 15:42:04 +0900

    router 변경

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 
 
 import './App.css';
-import { Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HeaderNav from 'components/Header/HeaderNav';
 import { AuthContextProvider } from 'components/context/AuthContext';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import NotFound from 'pages/NotFound';
+import Home from 'pages/Home';
+import AllProducts from 'pages/products/AllProducts';
+import MyCart from 'pages/carts/MyCart';
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-        <HeaderNav />
-        <Outlet />
+        <BrowserRouter>
+          <HeaderNav />
+          <Routes>
+            <Route path="/" exact element={<Home />}></Route>
+            <Route path="/products" element={<AllProducts />}></Route>
+            <Route path="/carts" element={<MyCart />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
+        {/* <Outlet /> */}
       </AuthContextProvider>
     </QueryClientProvider>
   );
